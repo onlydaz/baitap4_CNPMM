74c835c7183153c828fca36e5576ccdb38b667f8
onlydaz <163791995+onlydaz@users.noreply.github.com>
2025-08-29 14:08:04 +0700

    first project

---
+++ ExpressJS01/src/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+//import các ngân cần dùng
+const express = require('express');
+const configViewEngine = require('./configs/viewEngine');
+const apiRoutes = require('./routes/api');
+const connection = require('./config/database');
+const getHomepage = require('./controllers/homecontroller');
+const cors = require('cors');
+
+//cấu hình app là express
+const app = express();
+
+//cấu hình port, nếu tim thay port trong env, không thi trả về 8888
+const port = process.env.PORT || 8888;
+
+app.use(cors()); //config cors
+app.use(express.json()); //config req.body cho json
+app.use(express.urlencoded({ extended: true })); // for form data
+configViewEngine(app); //config template engine
+
+//config route cho view ejs
+const webAPI = express.Router();
+webAPI.get('/', getHomepage);
+app.use('/', webAPI);
+
+//khai báo route cho API
+app.use('/v1/api', apiRoutes);
+
+(async () => {
+    try {
+        //kết nối database using mongoose
+        await connection();
+
+        //lắng nghe port trong env
+        app.listen(port, () => {
+            console.log(`Backend Nodejs App Listening on port ${port}`);
+        });
+    } catch (error) {
+        console.log(">> Error connect to DB: ", error);
+    }
+})();
