acc76990272c24caef93ffcf8519b85a23a32579
onlydaz <163791995+onlydaz@users.noreply.github.com>
2025-09-09 14:28:29 +0700

    update

--- ReactJS01/reactjs01/src/App.jsx
+++ ReactJS01/reactjs01/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import Header from "../components/layout/header";
+import axios from "../util/axios.customize";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../components/context/auth.context";
+import { Spin } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { setAuth, appLoading, setAppLoading } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchAccount = async () => {
+      setAppLoading(true);
+      const res = await axios.get("/v1/api/users");
+      if (res && res.message) {
+        setAuth({
+          isAuthenticated: true,
+          user: {
+            email: res.email,
+            name: res.name
+          }
+        });
+      }
+      setAppLoading(false);
+    };
+
+    fetchAccount();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {appLoading === true ? (
+        <div style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)"
+        }}>
+          <Spin />
+        </div>
+      ) : (
+        <div>
+          <Header />
+          <Outlet />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- ReactJS01/reactjs01/src/main.jsx
+++ ReactJS01/reactjs01/src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/global.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import RegisterPage from './pages/register.jsx';
+import UserPage from './pages/user.jsx';
+import HomePage from './pages/home.jsx';
+import LoginPage from './pages/login.jsx';
+import { AuthWrapper } from './components/context/auth.context.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: "user",
+        element: <UserPage />
+      },
+      {
+        path: "register",
+        element: <RegisterPage />
+      },
+      {
+        path: "login",
+        element: <LoginPage />
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthWrapper>
+      <RouterProvider router={router} />
+    </AuthWrapper>
+  </React.StrictMode>,
+);
